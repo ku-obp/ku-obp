854a0d243c65143502c48917e921917b9de1f8c5
김민재 <lake041@korea.ac.kr>
2023-10-14 01:34:51 +0900

    Environment Setup (#1)
    ## Motivation
    
    - 기본 환경 세팅
    
    ## Key Changes
    
    - Create Next App 내용 삭제
    - Headless UI로 shadcn/ui를 사용
    - Route Group으로 라우팅 폴더 조직화
    
    ## Note
    
    -

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="flex justify-center items-center h-full">
+      <p className="text-3xl md:text-5xl font-bold text-rose-500">
+        Open Boardgame Project
+      </p>
+    </div>
+  );
+}
