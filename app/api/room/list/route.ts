3eac15fe0c227fc3845d6e07377c18be08504995
lake041 <lake041@korea.ac.kr>
2023-10-25 10:32:12 +0900

    Fix: Server-side Exception 수정

--- app/api/room/list/route.ts
+++ app/api/room/list/route.ts
@@ -3,7 +3,16 @@ import { NextResponse } from "next/server";
 
 export async function POST(request: Request) {
   try {
-    const { gameName } = await request.json();
+    const body = await request.text();
+
+    let gameName;
+    try {
+      const json = JSON.parse(body);
+      gameName = json.gameName;
+    } catch (error) {
+      console.error("Invalid JSON:", error);
+      return NextResponse.json({ message: "Invalid JSON format" });
+    }
 
     const query = sql`SELECT * FROM rooms WHERE game = ${gameName}`;
     const rooms = await query;
@@ -14,8 +23,9 @@ export async function POST(request: Request) {
     });
   } catch (error) {
     console.log(error);
-    return NextResponse.json({ message: error });
+    // 'Error' 인스턴스인지 확인한 후 메시지를 추출합니다.
+    const errorMessage =
+      error instanceof Error ? error.message : "An unknown error occurred";
+    return NextResponse.json({ message: errorMessage });
   }
-
-  return NextResponse.json({ message: "success" });
 }
