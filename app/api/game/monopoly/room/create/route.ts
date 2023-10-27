15ba1388f9a339cda91efd37e264cc2c199ca6f4
윤병익 <yeun0908@gmail.com>
2023-10-27 11:57:25 +0900

    further work 3

---
+++ app/api/game/monopoly/room/create/route.ts
@@ -0,0 +1,45 @@
+import { kv } from "@vercel/kv";
+import { v4 as uuidv4 } from "uuid";
+import { NextResponse } from "next/server";
+
+export async function POST(request: Request) {
+  const body = await request.text();
+
+  let hostEmail, hostColor;
+  try {
+    const json = JSON.parse(body);
+    hostEmail = json.hostEmail;
+    hostColor = json.hostColor;
+  } catch (error) {
+    console.error("Invalid JSON:", error);
+    return NextResponse.json({ message: "Invalid JSON format" });
+  }
+  const opponentColor = hostColor === "w" ? "b" : "w";
+
+  const roomId = uuidv4();
+  const roomKey = `monopoly:${roomId}`;
+  const roomStatus = {
+    roomId,
+    hostEmail,
+    hostColor,
+    opponentEmail: "",
+    opponentColor,
+    history: [startFen],
+    turnIndex: 0,
+    turnColor: "w",
+    lastMoveFrom: "",
+    lastMoveTo: "",
+    isStarted: false,
+    isEnd: false,
+  };
+  try {
+    await kv.set(roomKey, JSON.stringify(roomStatus), { ex: 100, nx: true }); // 1시간 후 만료
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({ message: "Failed to Create Room" });
+  }
+  return NextResponse.json({
+    message: "Room Created Successfully",
+    roomStatus,
+  });
+}
