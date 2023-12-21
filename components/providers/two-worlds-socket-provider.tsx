353e28bbf3b4eefd9a46d3b965d6def97c833796
윤병익 <yeun0908@gmail.com>
2023-12-21 09:46:17 +0900

    Gamma (#31)
    ## Motivation
    
    -
    
    ## Key Changes
    
    -
    
    ## Note
    
    -

--- components/providers/two-worlds-socket-provider.tsx
+++ components/providers/two-worlds-socket-provider.tsx
@@ -194,24 +194,6 @@ export const TwoWorldsProvider = ({
       console.log(`${playerEmail} Failed to join the room: ${msg}`);
     });
 
-    socket.on("joinSucceed", () => {
-      console.log('Succeeded to join the room')
-    })
-
-    socket.on("connect", () => {
-      console.log(`${playerEmail} Connected to Socket.io server`);
-      socket.emit("joinRoom", { roomId: params.roomId });
-    });
-
-
-    socket.on("disconnect", () => {
-      console.log("Disconnected from Socket.io server");
-    });
-
-    socket.on("notifyRoomStatus", (playerEmails: string[], isEndedStringified: string) => {
-      const isEnded = JSON.parse(isEndedStringified) as boolean
-      dispatch(notifyRoomStatus({playerEmails,isEnded}))
-    })
     socket.on("refreshGameState", (playerEmails: string[], isEndedStringified: string, playerStateStrings: string[], cellIds: number[], rawProperties: string, nowInTurn: number, govIncome: number, charityIncome: number, remainingCatastropheTurns: number, remainingPandemicTurns: number, doublesCount: number, diceCache: number, chanceId: string, prompt: string, quirkOfFateDiceCache: number) => {
       const isEnded = JSON.parse(isEndedStringified) as boolean
       console.log(playerStateStrings.length)
@@ -255,14 +237,13 @@ export const TwoWorldsProvider = ({
       console.log(propertiesJSON)
 
       const pairs = cellIds.map((cellId) => [cellId, parseProperty(propertiesJSON[`cell${cellId}`] as string)] as [number,PropertyType])
-      const properties = new Map<number,PropertyType>(pairs)
 
       dispatch(refreshGameState(
         {
           playerEmails: playerEmails,
           isEnded: isEnded,
           gs: {
-            playerStates: playerStates, properties: properties, nowInTurn: nowInTurn,
+            playerStates: copy(playerStates), properties: new Map<number,PropertyType>(pairs), nowInTurn: nowInTurn,
             govIncome: govIncome,
             charityIncome: charityIncome,
             remainingCatastropheTurns: remainingCatastropheTurns,
@@ -280,8 +261,27 @@ export const TwoWorldsProvider = ({
       ))
     })
 
+    socket.on("joinSucceed", () => {
+      console.log('Succeeded to join the room')
+    })
+
+    socket.on("connect", () => {
+      console.log(`${playerEmail} Connected to Socket.io server`);
+      socket.emit("joinRoom", { roomId: params.roomId });
+    });
+
+
+    socket.on("disconnect", () => {
+      console.log("Disconnected from Socket.io server");
+    });
+
+    socket.on("notifyRoomStatus", (playerEmails: string[], isEndedStringified: string) => {
+      const isEnded = JSON.parse(isEndedStringified) as boolean
+      dispatch(notifyRoomStatus({playerEmails,isEnded}))
+    })
     
     socket.on("updateGameState", (playerStateStrings: string[], cellIds: number[], rawProperties: string, nowInTurn: number, govIncome: number, charityIncome: number, remainingCatastropheTurns: number, remainingPandemicTurns: number, qofDiceCache: number) => {
+
       console.log(playerStateStrings.length)
       const playerStates: PlayerType[] = playerStateStrings.map((raw) => {
         const parsed = JSON.parse(raw)
@@ -318,8 +318,8 @@ export const TwoWorldsProvider = ({
           remainingJailTurns: remainingJailTurns as number
         } as PlayerType
       })
-
       const propertiesJSON = JSON.parse(rawProperties)
+
       console.log(propertiesJSON)
 
       const pairs = cellIds.map((cellId) => [cellId, parseProperty(propertiesJSON[`cell${cellId}`] as string)] as [number,PropertyType])
